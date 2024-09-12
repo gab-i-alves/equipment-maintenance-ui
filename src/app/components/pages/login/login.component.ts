274b6a06f84c3c5eca64e8e17e76258e810a9c3b
Giovanni Tenereli <giovanni.tenereli@cogecom.com.br>
2024-09-12 13:12:20 -0300

    Alteções no login

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,22 +1,21 @@
 import { Component } from '@angular/core';
-import User from '../../../../user';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email:string = '';
+  password:string = '';
+  submiting:boolean = false;
 
-  enviar(senha: string, email:string){
-    console.log("Botão" + email + senha);
-    let user: User = new User(email, senha);
-    this.users.push(user);
+  loginAction(){
+    console.log(this.email + "--" + this.password);
+    
   }
-
-  users: Array<User> = [];
-
 }
