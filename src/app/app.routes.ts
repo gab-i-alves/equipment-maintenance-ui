d39f0bd1538ec5dec1bcd7898dc1fd5067601e54
Diego Alves <diegoalvesdc12@gmail.com>
2024-09-14 16:34:01 -0300

    Implementa Pagina Inicial do Cliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/pages/login/login.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
     },
 ];
