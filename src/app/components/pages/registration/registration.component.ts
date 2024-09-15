ecc73d883c276d6f23b1f9a011ec74fe2bd4112f
gab-i-alves <gab.alvesnacimento@gmail.com>
2024-09-15 00:15:03 -0300

    arquivo .ts da tela de cadastro

---
+++ src/app/components/pages/registration/registration.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent {
+
+  onSubmit() {
+    console.log('Form submitted!');
+  }
+}
