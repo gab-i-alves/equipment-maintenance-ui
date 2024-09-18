052845d5e83793250b996d482eb4f0b32d5c76c0
gabisre <gabirech05@gmail.com>
2024-09-18 11:35:42 -0300

    validações

--- src/app/components/pages/registration/registration.component.ts
+++ src/app/components/pages/registration/registration.component.ts
@@ -10,8 +10,49 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  name: string = '';
+  email: string = '';
+  cpf: string = '';
+  phone: string = '';
+  cep: string = '';
+  state: string = '';
+  city: string = '';
+  logradouro: string = '';
+  number: string = '';
+  complement: string = '';
+  submiting: boolean = false;
 
   onSubmit() {
-    console.log('Form submitted!');
+    if (this.isFormValid()){
+      console.log('Formulário submetido com dados: ', {
+        nome: this.name,
+        email: this.email,
+        cpf:this.cpf,
+        telefone: this.phone,
+        cep: this.cep,
+        estado: this.state,
+        cidade: this.city,
+        logradouro: this.logradouro,
+        número: this.number,
+        complemento: this.complement
+      });
+    } else {
+      console.log('Formulário com erros');
+    }
+  }
+
+  isFormValid(): boolean {
+    return (
+      this.name.trim() !== '' &&
+      this.email.trim() !== '' &&
+      this.email.includes('@') &&
+      this.cpf.trim().length === 14 &&
+      this.phone.trim().length >= 11 &&
+      this.cep.trim().length === 9 &&
+      this.state.trim() !== '' &&
+      this.city.trim() !== '' &&
+      this.logradouro.trim() !== '' &&
+      this.number.trim() !== ''
+    );
   }
 }
