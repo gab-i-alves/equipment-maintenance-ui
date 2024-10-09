bda8c27c49cfc2203fa355bb78707c88a34d388f
gab-i-alves <gab.alvesnacimento@gmail.com>
2024-10-09 18:00:15 -0300

    mostrar categorias dos equipamentos

---
+++ src/app/components/employee/pages/list-categories/list-categories.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { EquipmentCategory } from '../../../../models/equipment-category/equipment-category.model';
+import { EquipmentCategoryService } from '../../../../services/equipment-category/equipment-category.service';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { EmployeeSidebarComponent } from '../../employee-sidebar/employee-sidebar.component';
+
+@Component({
+  selector: 'app-list-categories',
+  standalone: true,
+  imports: [EmployeeSidebarComponent, FormsModule, RouterModule, CommonModule],
+  templateUrl: './list-categories.component.html',
+  styleUrl: './list-categories.component.css'
+})
+export class ListarCategoriaComponent implements OnInit {
+  categorias: EquipmentCategory[] = [];
+
+  constructor(private categoriaService: EquipmentCategoryService) {}
+
+  ngOnInit(): void {
+    this.categorias = this.categoriaService.listarTodos();
+  }
+
+  removerCategoria(id: number): void {
+    if (confirm('Deseja realmente remover esta categoria?')) {
+      this.categoriaService.remover(id);
+      this.categorias = this.categoriaService.listarTodos();
+    }
+  }
+}
