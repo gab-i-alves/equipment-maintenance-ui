bf2c17a3594c931c911852a1ab7aa5fc8d7dbeb7
GiovanniT <giovannilmtenereli@gmail.com>
2024-11-25 19:10:35 -0300

    Inicio da conexão com o back para o login

--- src/app/components/customer/pages/login/login.component.ts
+++ src/app/components/customer/pages/login/login.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { LoginService } from '../../../../services/login/login.service';
+import { Login } from '../../../../models/login/login';
 
 @Component({
   selector: 'app-login',
@@ -20,23 +22,19 @@ export class LoginComponent {
 
   loginConditions: [boolean, boolean] = [false, false]; //[Campos não preenchidos, Login incorreto]
 
-  constructor(private router: Router, private AccountService: AccountService, private http: HttpClient) {}
+  constructor(private router: Router, private accountService: AccountService, private loginService: LoginService, private http: HttpClient) {}
 
   loginAction(){
-
-    this.loginConditions = this.AccountService.login(this.email, this.password);
-
     if(!this.loginConditions[0] && !this.loginConditions[1]){
-
-      if(localStorage.getItem('user') == 'customer'){
-
-        this.router.navigate(['/home']);
-
-      }else{
-
-        this.router.navigate(['/employee-home']);
-        
-      }
+      console.log("Login")
+      this.loginService.login(new Login(this.email, this.password)).subscribe(
+        (response: Login | null) => {
+          console.log('Login feito com sucesso:', response);
+        },
+        (error) => {
+          console.error('Erro no login:', error);
+        }
+      );
       
     }
 
