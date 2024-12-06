ae1f3cf6a7b66accc44bdc9077acb745affe76ed
GiovanniT <giovannilmtenereli@gmail.com>
2024-12-06 15:42:23 -0300

    Correções para o login de usuários inativos

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -21,6 +21,8 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
   submiting: boolean = false;
+  errorMessage: string = '';
+  hasError: boolean = false;
   loginConditions: [boolean, boolean] = [false, false]; //[Campos não preenchidos, Login incorreto]
 
   constructor(
@@ -30,33 +32,40 @@ export class LoginComponent {
   ) {}
 
   loginAction() {
-    this.loginConditions[0] = this.email === '' || this.password === '';
-    if (this.loginConditions[0]) return;
+    if (this.email === '' || this.password === '') {
+      this.handleError('Preencha corretamente ambos os campos');
+      return;
+    }
 
     this.loginService.login(new Login(this.email, this.password)).subscribe(
       (response: any) => this.handleLoginResponse(response),
-      (error) => console.error('Erro no login:', error)
+      (error) => this.handleError('Erro no login: ' + error)
     );
   }
 
   handleLoginResponse(response: any) {
-    if (response == null) {
-      this.loginConditions[1] = true;
+    if (response == null || !response.ativo) {
+      this.handleError('E-mail ou senha incorretos');
       return;
     }
 
     const perfil = response.tipoPerfil.descricao;
     this.loginConditions[1] = false;
-    
+
     if (perfil === 'Cliente') {
-      const cliente : Customer = response;
+      const cliente: Customer = response;
       this.authService.login(cliente);
       this.router.navigate(['/home']);
     } else {
-      const funcionario : Employee = response;
+      const funcionario: Employee = response;
       this.authService.login(funcionario);
       this.router.navigate(['/employee-home']);
     }
+  }
+
+  handleError(message: string) {
+    this.hasError = true;
+    this.errorMessage = message;
   }
 
   createAcount() {
